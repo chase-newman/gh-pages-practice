bcc4ce985efdfca00dea8445ba183867d8449a57
EC2 Default User <ec2-user@ip-172-31-90-173.ec2.internal>
2020-05-17 15:33:23 +0000

    Updates

---
+++ precache-manifest.f614c1df675cec92eb17769f9ea01946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5f7a1c8dd3e6d32c6a8e815419712c",
+    "url": "/gh-pages-practice/index.html"
+  },
+  {
+    "revision": "cc6d0754b1845ec21154",
+    "url": "/gh-pages-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb67c2d5dbb26ae1f16",
+    "url": "/gh-pages-practice/static/js/2.f621f8cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-practice/static/js/2.f621f8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6d0754b1845ec21154",
+    "url": "/gh-pages-practice/static/js/main.2172d1d6.chunk.js"
+  },
+  {
+    "revision": "ba1ef48278df57a4ff27",
+    "url": "/gh-pages-practice/static/js/runtime-main.e98bbcb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
